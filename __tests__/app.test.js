ea0bee7376675452d4b88f2aa3b3c0d6553fbe4e
Carson Culler <carsonculler@admins-MBP.hsd1.ca.comcast.net>
2023-08-09 12:02:30 -0700

    make test fail to test workflow

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,6 @@
 // Add the above comment to all jest files so they don't fail linting due to no-undef
 describe('app.js testing suite', () => {
   test('Smoke test', () => {
-    expect(1).toBe(1)
+    expect(1).toBe(0)
   })
 })
