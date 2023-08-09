167454d8fb251c0ce2d7d3fad178d71e17c3ea99
Carson Culler <carsonculler@admins-MBP.hsd1.ca.comcast.net>
2023-08-09 11:27:10 -0700

    fixed linting errors in jest config file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  testEnvironment: 'node', // or 'jsdom' for front-end projects
+  testEnvironment: 'node' // or 'jsdom' for front-end projects
   // Add any other Jest configuration options here as needed
-};
+}
