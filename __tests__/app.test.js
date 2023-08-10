2afc76a88e4ffae1441e428e274e0244047aad4f
cfculler <carsonculler2020@gmail.com>
2023-08-10 16:37:29 -0700

    temp change to tests to test coverage badge

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
-const { app } = require('../app.js')
+// const { app } = require('../app.js')
 /* eslint-env jest */
 // Add the above comment to all jest files so they don't fail linting due to no-undef
 describe('app.js testing suite', () => {
-  console.log(app)
+  // console.log(app)
 
   test('Smoke test', () => {
     expect(1).toBe(1)
