51f14adc96d19800d8f1f2277ee939f6e7fe4f37
keoniburns <keoni.burns@liatrio.com>
2023-08-15 15:38:58 -0700

    added first iteration of the group insertion function for the database

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const { App } = require('@slack/bolt')
 const { mongoose } = require('mongoose')
+const { addToDB } = require('./db')
 require('dotenv').config()
 
 // setting up app
@@ -23,16 +24,28 @@ mongoose.connect(`mongodb://${db}:27017/endybot`).then(
   }
 )
 
+const fakeGroup = {
+  name: 'test-group',
+  contributors: ['keoni', 'mikayla', 'carson'],
+  subscribers: ['josh'],
+  postTime: 5,
+  channel: 'fake-channel'
+}
+
 app.command('/endybot', async ({ command, ack, respond }) => {
   await ack()
 
   switch (command) {
-    case 'create':
+    case 'create':{
       // send user the form (return filled out form)
       // parse form (filled out form -> function -> json object {groupName, contributor list, subscriber list, postTime, channel})
       // use form input to create group in db (json object from above -> function -> json obj {groupName, contributors list, subscribers list, channel, success})
-      // respond to user ("added group x with users a, b, c to channel y" or "failed to create group x")
-      break
+
+      const groupID = await addToDB(fakeGroup)
+
+      if (groupID) { return 1 } else { return null }
+    }
+    // respond to user ("added group x with users a, b, c to channel y" or "failed to create group x")
     default:
       respond(`Command ${command} not found`)
       break
--- src/db.js
+++ src/db.js
@@ -1,1 +1,14 @@
 // Put all functions interacting with the database here
+const Group = require('../db-schemas/group')
+
+async function addToDB (groupJson) {
+  try {
+    const inserted = await Group.create(groupJson)
+    return inserted._id
+  } catch (err) {
+    console.error('error adding to the database: ', err)
+    return null
+  }
+}
+
+module.exports = { addToDB }
