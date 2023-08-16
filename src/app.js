7ccf32c96c99111832e65d2796f4961690a39994
cfculler <carsonculler2020@gmail.com>
2023-08-16 14:21:36 -0700

    Added a function to list all the groups

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const { App } = require('@slack/bolt')
-const { mongoose } = require('mongoose')
 require('dotenv').config()
+const database = require('./db.js')
 
 // setting up app
 const app = new App({
@@ -12,27 +12,20 @@ const app = new App({
 // starting app
 app.start(process.env.PORT || 3000).then(console.log('⚡️ Bolt app is currently running!'))
 
-// connect to database
-const db = process.env.DEV == 1 ? 'db' : '127.0.0.1'
-mongoose.connect(`mongodb://${db}:27017/endybot`).then(
-  () => {
-    console.log('Successfully connected to db')
-  },
-  err => {
-    console.log('Could not connect to db. Error: ' + err)
-  }
-)
-
 app.command('/endybot-dev', async ({ command, ack, respond }) => {
   await ack()
-
-  switch (command) {
+  switch (command.text) {
     case 'create':
       // send user the form (return filled out form)
       // parse form (filled out form -> function -> json object {groupName, contributor list, subscriber list, postTime, channel})
       // use form input to create group in db (json object from above -> function -> json obj {groupName, contributors list, subscribers list, channel, success})
       // respond to user ("added group x with users a, b, c to channel y" or "failed to create group x")
       break
+    case 'list': {
+      const data = await database.listGroups()
+      respond(`${data}`)
+      break
+    }
     default:
       respond(`Command ${command.text} not found`)
       break
