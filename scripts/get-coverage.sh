cbd02f52e4086af997d3356870da1328b3f69d05
Nicholas Shaddox <Nicholas.Shaddox@liatrio.com>
2023-09-20 09:35:47 -0700

    fix: remove update script and old badge

--- scripts/get-coverage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TEMP=`npm run coverage 2>/dev/null | tail | grep Lines | awk '{print $3}'`
-
-# Print the coverage percentage
-echo "$TEMP"
-
-# Run with: ./scripts/get-coverage.sh 2>/dev/null
